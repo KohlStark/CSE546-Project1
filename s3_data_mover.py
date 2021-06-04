228166b4481a206a5954d67a10f4f620e7e8d587
KohlStark <kohlstark@yahoo.com>
2021-06-04 12:22:36 -0700

    edited ec2 manager and s3 mover. added node code

--- s3_data_mover.py
+++ s3_data_mover.py
@@ -1,16 +1,19 @@
 import boto3
+import sys
+images_path = "/home/ubuntu/upload_images/"
+#download_path = "/Users/KohlStark/Documents/CSE546/CSE546-Project1"
 
-images_path = "/Users/KohlStark/Documents/CSE546/CSE546-Project1/imagenet-100/"
+firstarg=sys.argv[1]
 
-download_path = "/Users/KohlStark/Documents/CSE546/CSE546-Project1"
 
-client = boto3.client('s3', region_name='us-east-2')
+client = boto3.client('s3', region_name='us-east-1')
 
 def uploadPicture(picPath, bucketName, s3Name):
     client.upload_file(picPath, bucketName, s3Name)
+    print("Success uploading", picPath, " to S3")
     
 def downloadPicture(bucketName, s3Name, picPath):
     client.download_file(bucketName, s3Name, picPath)
 
-#uploadPicture(images_path + 'test_0.JPEG', 'cse-546-picture-files', 'test_0.JPEG')
-downloadPicture('cse-546-picture-files', 'test_0.JPEG', download_path + 'test_0.JPEG')
+uploadPicture(images_path + firstarg, 'cse-546-picture-files', firstarg)
+#downloadPicture('cse-546-picture-files', 'test_0.JPEG', download_path + 'test_0.JPEG')
