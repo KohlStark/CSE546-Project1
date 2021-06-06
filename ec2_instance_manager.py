7f94a1eb48fbf84c223a1ac1cd82bb8e2ab54ad7
KohlStark <kohlstark@yahoo.com>
2021-06-05 19:11:33 -0700

    Removed terminate instances, added start & bulk

--- ec2_instance_manager.py
+++ ec2_instance_manager.py
@@ -43,6 +43,18 @@ def bulk_create_instances(num):
     for i in range(num):   
         create_instance() 
 
+def start_instance(instance_id):
+    print('Starting instance with ID:',instance_id)
+    ec2 = boto3.client("ec2", region_name="us-east-1")
+    response = ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
+    print(response)
+    #update_instance_state(instanceid,1) # setting value for this instance as one in s3 bucket.
+   
+def bulk_start_instances(instance_ids):
+    instance_ids.remove("i-0c6fe4c893fc48b1d")
+    print("Starting these instances", instance_ids)
+    for i in instance_ids:
+        start_instance(i)
 
 def stop_instance(instance_id):
     print("Stopping instance:", instance_id)
@@ -50,26 +62,28 @@ def stop_instance(instance_id):
     response = ec2_client.stop_instances(InstanceIds=[instance_id])
     print(response)
 
-
-def terminate_instance(instance_id):
-    print("Terminating instance:", instance_id)
-    ec2_client = boto3.client("ec2", region_name="us-east-1")
-    response = ec2_client.terminate_instances(InstanceIds=[instance_id])
-    print(response)
-
-def stop_and_terminate(instance_id):
-    stop_instance(instance_id)
-    terminate_instance(instance_id)
-
-def bulk_stop_and_terminate(instance_ids):
+def bulk_stop_instances(instance_ids):
     instance_ids.remove("i-0c6fe4c893fc48b1d")
-    print("Deleting these instances", instance_ids)
+    print("Stopping these instances", instance_ids)
     for i in instance_ids:
         stop_instance(i)
-        terminate_instance(i)
 
+# def terminate_instance(instance_id):
+#     print("Terminating instance:", instance_id)
+#     ec2_client = boto3.client("ec2", region_name="us-east-1")
+#     response = ec2_client.terminate_instances(InstanceIds=[instance_id])
+#     print(response)
 
+# def stop_and_terminate(instance_id):
+#     stop_instance(instance_id)
+#     terminate_instance(instance_id)
 
+# def bulk_stop_and_terminate(instance_ids):
+#     instance_ids.remove("i-0c6fe4c893fc48b1d")
+#     print("Deleting these instances", instance_ids)
+#     for i in instance_ids:
+#         stop_instance(i)
+#         terminate_instance(i)
 
 def get_running_instances():
     instance_list = []
@@ -94,13 +108,11 @@ def get_running_instances():
             
 
 #create_key_pair()
-create_instance()
-#get_public_ip("i-096b2fa8bbc536c4b")
-
+#create_instance()
+#bulk_create_instances(19)
 
 #stop_instance("i-096b2fa8bbc536c4b")
-#terminate_instance("i-096b2fa8bbc536c4b")
-
-#bulk_create_instances(5)
 #instance_list = get_running_instances()
-#bulk_stop_and_terminate(instance_list)
+#bulk_stop_instances(instance_list)
+
+
