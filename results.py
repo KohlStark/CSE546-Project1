4ba4dd3993b8903f4c2682d24fe3ef583d1264e9
Ian Bolton <ipbolton@asu.edu>
2021-06-17 18:35:31 -0700

    building output string
    :)

--- results.py
+++ results.py
@@ -10,18 +10,19 @@ firstarg = sys.argv[1]
 def printOutputs(num_requests):
     #infinite loop to continuously poll the response queue
     requests_met = 0
+    outputs = ""
     while True:
         if requests_met == num_requests:
+            print(outputs)
+            sys.stdout.flush()
             return
         else:
             for msg in queue.receive_messages(MaxNumberOfMessages=10):
                 # save body (the pair) and message attribute (the name)
                 response_string = msg.body
                 response_name = str(msg.body).split(", ")
-                # print so it goes to stdout, which will route it to the response_queue_poller.stdout.on call
-                # in test_app.js
-                print("(" + str(response_name[0][:-5]) + ", " + str(response_name[1]) + ")")
-                sys.stdout.flush()
+                # build output string
+                outputs += ("(" + str(response_name[0][:-5]) + ", " + str(response_name[1]) + ")\n")
 
                 # remove message from response queue after processing it
                 msg.delete()
