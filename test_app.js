034e095c11eb2928dc572c9c2f0804ab646e3896
KohlStark <kohlstark@yahoo.com>
2021-06-17 17:20:43 -0700

    app

--- test_app.js
+++ test_app.js
@@ -4,42 +4,15 @@ const multer = require('multer');
 const server = express();
 const PORT = 3000;
 const {spawn} = require('child_process');
+const { response } = require('express');
 
 
 const controller = spawn('python', ['/home/ubuntu/ec2_controller/controller.py']);
 
-// save classification results
-//var outputs = []
-
-// call response queue polling loop
-const response_queue_poller = spawn('python', ['/home/ubuntu/s3_uploader/results.py'])
-// on poller printing (I think), take that data and save it in dictionary
-// key: Image name, value: output pair, e.g. (test_0, bathtub)
-response_queue_poller.stdout.on('data', function(data) {
-        console.log("DATA");
-        console.log(data.toString());
-        //var str = data.toString(), dict_vals = str.split(":");
-        //outputs.push({
-        //      key: dict_vals[0],
-        //      value: dict_vals[1]
-        //});
-        //console.log(dict_vals);
-});
-
-//console.log(outputs);
-
-response_queue_poller.stderr.on('data', function(data) {
-        console.log("ERR");
-        console.log(data.toString());
-});
-
-response_queue_poller.stdout.on('end', function(data) {
-        console.log(data.toString());
-});
-
 controller.stdout.on('data', function(data) {
   console.log(data.toString());
 });
+
 
 // uploaded images are saved in the folder "/upload_images"
 const upload = multer({dest: __dirname + '/upload_images'});
@@ -49,7 +22,7 @@ server.use(express.static('public'));
 // "myfile" is the key of the http payload
 server.post('/', upload.single('myfile'), function(request, respond) {
   if(request.file) console.log(request.file);
-  console.log("Uploading " + request.file.originalname + " to S3")
+  console.log("Uploading " + request.file.originalname + " to S3");
 
   // Send the file to S3
   // const s3_python = spawn('python', ['/home/ubuntu/s3_uploader/s3_data_mover.py', request.file.originalname]);
@@ -72,6 +45,29 @@ server.post('/', upload.single('myfile'), function(request, respond) {
 
   respond.end(request.file.originalname + ' uploaded!');
 });
+
+server.use(express.urlencoded());
+server.use(express.json());
+
+server.post('/response_queue', function(request, respond) {
+  console.log("Starting response queue poller", request.body.num);
+  const response_queue_poller = spawn('python', ['/home/ubuntu/s3_uploader/results.py', request.body.num]);
+// on poller printing (I think), take that data and save it in dictionary
+// key: Image name, value: output pair, e.g. (test_0, bathtub)
+  response_queue_poller.stdout.on('data', function(data) {
+        console.log("DATA");
+        console.log(data.toString());
+  //response.end()
+
+ });
+  
+
+});
+
+// server.get("/", function(req, res){
+//   path = "/home/ubuntu/outputs.txt"
+//   res.sendFile(path);
+// });
 
 // You need to configure node.js to listen on 0.0.0.0 so it will be able to accept connections on all the IPs of your machine
 const hostname = '0.0.0.0';
